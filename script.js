5a91b9415dffc6a7ce5707e7d7b84843a042ca60
DaniyaI-Aslam <daniyalaslam54@gmail.com>
2022-11-22 15:28:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let Video = null; 
+let Canvas = null; 
+let Contex = null; 
+
+
+function main(){
+    Canvas = document.getElementById("idCanvas");
+    Contex = Canvas.getContext("2d");
+        Canvas.width = window.innerWidth;
+        Canvas.height = window.innerHeight;
+    let promise = navigator.mediaDevices.getUserMedia({video:true})
+    promise.then(function(signal){
+    Video = document.createElement("video");
+    Video.srcObject = signal;
+    Video.play();
+    Video.onloadeddata = function(){
+        updateCanvas();
+    }
+    }).catch(function(err){
+        alert("Camera Error:"+err);
+    });
+    
+}
+function updateCanvas(){
+    Contex.drawImage(Video,0,0);
+    window.requestAnimationFrame(updateCanvas)
+}
